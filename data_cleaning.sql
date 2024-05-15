a5f6ec5b67e8ab7594429f4ca2ffdc4257f8d270
valmaqueda <113254902+valmaqueda@users.noreply.github.com>
2024-05-15 14:40:27 -0600

    Update data_cleaning.sql

--- data_cleaning.sql
+++ data_cleaning.sql
@@ -6,16 +6,6 @@ CREATE SCHEMA cleaning;
 CREATE TABLE cleaning.crime_data AS
 SELECT * FROM raw.crime_data;
 
-
---Consolidación de Códigos de Crímenes:
-UPDATE cleaning.crime_data
-SET crm_cd_desc = CASE
-    WHEN crm_cd_desc LIKE '%HOMICIDE%' THEN 'HOMICIDE'
-    WHEN crm_cd_desc LIKE '%THEFT%' OR crm_cd_desc LIKE '%ROBBERY%' THEN 'THEFT/ROBBERY'
-    WHEN crm_cd_desc LIKE '%ASSAULT%' THEN 'ASSAULT'
-    ELSE crm_cd_desc
-END;
-
 --Eliminación de Datos Irrelevantes pues no contienen datos
 ALTER TABLE cleaning.crime_data
 DROP COLUMN crm_cd_3,
@@ -39,13 +29,12 @@ SET
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
 -- Limpieza de las fechas
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
-  -- Actualización del formato de las columnas de fecha en la base de datos
+-- Actualización del formato de las columnas de fecha en la base de datos
 BEGIN;
 
-  -- Asumiendo que las fechas podrían estar en formatos mixtos, como 'YYYY-MM-DD' o 'MM/DD/YYYY'
 ALTER TABLE raw.crime_data
-ALTER COLUMN "Date Rptd" TYPE DATE USING TO_DATE("Date Rptd", 'YYYY-MM-DD'),
-ALTER COLUMN "DATE OCC" TYPE DATE USING TO_DATE("DATE OCC", 'YYYY-MM-DD');
+ALTER COLUMN date_rptd SET DATA TYPE TIMESTAMP,
+ALTER COLUMN date_occ SET DATA TYPE TIMESTAMP;
 
 COMMIT;
 
@@ -53,71 +42,70 @@ COMMIT;
 --  Limpieza de Time OCC
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
 -- Crear una función para convertir la hora
-
-CREATE OR REPLACE FUNCTION clean_time_occ(raw_time INT) RETURNS TIME AS $$
+CREATE OR REPLACE FUNCTION format_time_occ(raw_time INT) RETURNS TIME AS $$
 BEGIN
-    -- Si el número es de cuatro dígitos y válido como hora
-    IF raw_time BETWEEN 0 AND 2359 THEN
-        RETURN TO_CHAR(TO_TIMESTAMP(LPAD(raw_time::TEXT, 4, '0'), 'HH24MI'), 'HH24:MI')::TIME;
-    -- Si el número es de tres dígitos, asumir el primer dígito como hora y los dos últimos como minutos
-    ELSIF raw_time BETWEEN 100 AND 959 THEN
-        RETURN TO_CHAR(TO_TIMESTAMP(LPAD(raw_time::TEXT, 4, '0'), 'HH24MI'), 'HH24:MI')::TIME;
-    -- Si el número es de uno o dos dígitos, asumir como hora completa y minutos '00'
-    ELSIF raw_time BETWEEN 1 AND 99 THEN
-        RETURN TO_CHAR(TO_TIMESTAMP(LPAD(raw_time::TEXT, 4, '0'), 'HH24MI'), 'HH24:MI')::TIME;
-    -- Valores atípicos o incorrectos
-    ELSE
-        RETURN NULL;
-    END IF;
+    -- Convertir el entero a texto y luego formatear como tiempo
+    RETURN TO_TIMESTAMP(LPAD(raw_time::TEXT, 4, '0'), 'HH24MI')::TIME;
 END;
 $$ LANGUAGE plpgsql;
 
--- Agregar una nueva columna para el tiempo limpio
-ALTER TABLE raw.crime_data ADD COLUMN clean_time TIME;
+BEGIN;
 
--- Poblar la nueva columna con datos limpios
+ALTER TABLE raw.crime_data
+ADD COLUMN formatted_time_occ TIME;
+
 UPDATE raw.crime_data
-SET clean_time = clean_time_occ("TIME OCC");
+SET formatted_time_occ = format_time_occ(time_occ);
 
--- Verificar los datos
-SELECT "TIME OCC", clean_time FROM raw.crime_data LIMIT 10;
+ALTER TABLE raw.crime_data
+DROP COLUMN time_occ;
 
--- Después se puede eliminar la columna original y cambiar el nombre de la nueva
-ALTER TABLE raw.crime_data DROP COLUMN "TIME OCC";
-ALTER TABLE raw.crime_data RENAME COLUMN clean_time TO "TIME OCC";
+ALTER TABLE raw.crime_data
+RENAME COLUMN formatted_time_occ TO time_occ;
+
+COMMIT;
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
 -- Limpieza de area name
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
-UPDATE raw.crime_data
-SET "AREA NAME" = INITCAP("AREA NAME");
-UPDATE raw.crime_data
-SET "AREA NAME" = REGEXP_REPLACE("AREA NAME", '\s+', ' ', 'g');
 
 --Verifica que todos los nombres de área sean válidos y consultables, especialmente si se utilizan en reportes o interfaces de usuario.
-SELECT "AREA NAME", COUNT(*) AS count
+SELECT area_name, COUNT(*) AS count
 FROM raw.crime_data
-GROUP BY "AREA NAME"
+GROUP BY area_name
 ORDER BY count DESC;
-
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
 -- Limpieza de Vict Age, Vict Sex & Vict Desc
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
 -- Reemplazar 0 o valores nulos con NULL
 UPDATE raw.crime_data
-SET "Vict Age" = NULL
-WHERE "Vict Age" = 0 OR "Vict Age" IS NULL;
+SET vict_age = NULL
+WHERE vict_age = 0 OR vict_age IS NULL;
+
+UPDATE raw.crime_data
+SET vict_sex = CASE
+    WHEN UPPER(vict_sex) IN ('M', 'F', 'X') THEN UPPER(vict_sex)
+    ELSE 'X'
+END;
+
+-- Actualización de 'Vict Age' para manejar edades negativas
+-- Esta consulta reemplaza cualquier edad negativa con NULL
+UPDATE raw.crime_data
+SET vict_age = NULL
+WHERE vict_age < 0;
+
 
 -- Asegurar que solo M, F, o X estén permitidos, reemplazar otros con 'X'
 UPDATE raw.crime_data
-SET "Vict Sex" = UPPER("Vict Sex"),
-    "Vict Sex" = CASE
-        WHEN "Vict Sex" IN ('M', 'F', 'X') THEN "Vict Sex"
-        ELSE 'X'
-    END;
+SET vict_sex = CASE
+    WHEN UPPER(vict_sex) IN ('M', 'F', 'X') THEN UPPER(vict_sex)
+    ELSE 'X'
+END;
 
--- Creación de la tabla de descenso
+-- Creación de la tabla para catalogar la descendencia de las víctimas
+
+DROP TABLE IF EXISTS cleaning.vict_descent;
 CREATE TABLE cleaning.vict_descent (
     code CHAR(1),
     description VARCHAR(50)
@@ -125,167 +113,72 @@ CREATE TABLE cleaning.vict_descent (
 
 -- Insertar descripciones de cada código
 INSERT INTO cleaning.vict_descent (code, description) VALUES
-('O', 'Other'),
 ('H', 'Hispanic'),
-('B', 'Black'),
 ('W', 'White'),
-('C', 'Asian'),
+('B', 'Black'),
+('X', 'Not Specified'),  -- Asumimos que 'X' es usado para no especificado
+('O', 'Other'),
 ('A', 'South Asian'),
 ('K', 'Korean'),
 ('F', 'Filipino'),
-('X', 'Not Specified');
+('C', 'Asian'),
+('J', 'Category J'),  -- Descripción genérica, ajusta según necesidad
+('V', 'Category V'),  -- Descripción genérica, ajusta según necesidad
+('I', 'Category I'),  -- Descripción genérica, ajusta según necesidad
+('Z', 'Category Z'),  -- Descripción genérica, ajusta según necesidad
+('P', 'Category P'),  -- Descripción genérica, ajusta según necesidad
+('U', 'Category U'),  -- Descripción genérica, ajusta según necesidad
+('D', 'Category D'),  -- Descripción genérica, ajusta según necesidad
+('G', 'Category G'),  -- Descripción genérica, ajusta según necesidad
+('L', 'Category L'),  -- Descripción genérica, ajusta según necesidad
+('S', 'Category S');  -- Descripción genérica, ajusta según necesidad
 
--- Asegurar que todos los registros tengan un código válido, reemplazar vacíos con 'X'
+-- Asegurar que todos los registros tengan un código válido, reemplazar vacíos y no listados con 'X'
 UPDATE raw.crime_data
-SET "Vict Descent" = COALESCE("Vict Descent", 'X')
-WHERE "Vict Descent" IS NULL OR "Vict Descent" NOT IN (SELECT code FROM cleaning.vict_descent);
+SET vict_descent = COALESCE(vict_descent, 'X')
+WHERE vict_descent IS NULL OR vict_descent NOT IN (SELECT code FROM cleaning.vict_descent);
+
+UPDATE raw.crime_data
+SET vict_descent = 'X'
+WHERE vict_descent = '-';
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
 -- Limpieza de Premis Cd & Premis Desc
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
--- Creación de la tabla de códigos de localización
-CREATE TABLE cleaning.premis_codes (
-    premis_code INT PRIMARY KEY,
-    description VARCHAR(255)
-);
-
--- Insertar los códigos y descripciones de localización
-INSERT INTO cleaning.premis_codes (premis_code, description) VALUES
-(101, 'STREET'),
-(128, 'BUS STOP/LAYOVER'),
-(502, 'MULTI-UNIT DWELLING (APARTMENT, DUPLEX, ETC)'),
-(405, 'CLOTHING STORE'),
-(102, 'SIDEWALK'),
-(501, 'SINGLE FAMILY DWELLING'),
-(248, 'CELL PHONE STORE'),
-(750, 'CYBERSPACE'),
-(203, 'OTHER BUSINESS'),
-(108, 'PARKING LOT'),
-(751, 'WEBSITE'),
-(605, 'AUTOMATED TELLER MACHINE (ATM)'),
-(504, 'OTHER RESIDENCE'),
-(404, 'DEPARTMENT STORE'),
-(221, 'PUBLIC STORAGE'),
-(707, 'GARAGE/CARPORT'),
-(209, 'EQUIPMENT RENTAL'),
-(726, 'POLICE FACILITY'),
-(702, 'OFFICE BUILDING/OFFICE'),
-(801, 'MTA BUS'),
-(729, 'SPECIALTY SCHOOL/OTHER'),
-(737, 'SKATING RINK'),
-(602, 'BANK'),
-(720, 'JUNIOR HIGH SCHOOL'),
-(124, 'BUS STOP'),
-(103, 'ALLEY'),
-(122, 'VEHICLE, PASSENGER/TRUCK'),
-(116, 'OTHER/OUTSIDE'),
-(506, 'ABANDONED BUILDING ABANDONED HOUSE'),
-(212, 'TRANSPORTATION FACILITY (AIRPORT)'),
-(505, 'MOTEL'),
-(701, 'HOSPITAL'),
-(710, 'OTHER PREMISE'),
-(120, 'STORAGE SHED'),
-(145, 'MAIL BOX'),
-(735, 'NIGHT CLUB (OPEN EVENINGS ONLY)'),
-(503, 'HOTEL'),
-(104, 'DRIVEWAY'),
-(222, 'LAUNDROMAT'),
-(119, 'PORCH, RESIDENTIAL'),
-(406, 'OTHER STORE');
+-- Supongamos que elegimos el código 999 para 'UNKNOWN'
+-- Primero, asegurarse de que el código 999 no está ya en uso para otra descripción
+UPDATE raw.crime_data
+SET premis_cd = 999
+WHERE premis_desc = 'UNKNOWN';
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
 -- Limpieza de Weapon Used Cd & Weapon Desc
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
--- Creación de la tabla de códigos de armas
-CREATE TABLE cleaning.weapon_codes (
-    weapon_code INT PRIMARY KEY,
-    description VARCHAR(255)
-);
-
--- Insertar los códigos y descripciones de armas
-INSERT INTO cleaning.weapon_codes (weapon_code, description) VALUES
-(400, 'STRONG-ARM (HANDS, FIST, FEET OR BODILY FORCE)'),
-(307, 'VEHICLE'),
-(500, 'UNKNOWN WEAPON/OTHER WEAPON'),
-(511, 'VERBAL THREAT'),
-(106, 'UNKNOWN FIREARM'),
-(102, 'HAND GUN'),
-(223, 'UNKNOWN TYPE CUTTING INSTRUMENT'),
-(217, 'SWORD'),
-(308, 'STICK'),
-(304, 'CLUB/BAT'),
-(204, 'FOLDING KNIFE'),
-(513, 'STUN GUN'),
-(207, 'OTHER KNIFE'),
-(109, 'SEMI-AUTOMATIC PISTOL'),
-(113, 'SIMULATED GUN'),
-(205, 'KITCHEN KNIFE'),
-(515, 'PHYSICAL PRESENCE'),
-(512, 'MACE/PEPPER SPRAY'),
-(306, 'ROCK/THROWN OBJECT'),
-(104, 'SHOTGUN'),
-(310, 'CONCRETE BLOCK/BRICK'),
-(200, 'KNIFE WITH BLADE 6INCHES OR LESS'),
-(103, 'RIFLE'),
-(114, 'AIR PISTOL/REVOLVER/RIFLE/BB GUN'),
-(311, 'HAMMER'),
-(219, 'SCREWDRIVER'),
-(101, 'REVOLVER');
-
--- Actualizar registros donde no hay descripción de arma
+-- Asignar un código y descripción estándar para armas no especificadas
 UPDATE raw.crime_data
-SET "Weapon Desc" = (
-    SELECT description
-    FROM cleaning.weapon_codes
-    WHERE weapon_code = raw.crime_data."Weapon Used Cd"
-)
-WHERE "Weapon Desc" IS NULL AND "Weapon Used Cd" IS NOT NULL;
-
--- Establecer 'UNKNOWN WEAPON/OTHER WEAPON' donde no hay código de arma
+SET weapon_used_cd = 999, -- Usar 999 o cualquier otro código que no esté en uso
+    weapon_desc = 'NO WEAPON REPORTED'
+WHERE weapon_used_cd IS NULL AND weapon_desc IS NULL;
+-- Normalizar las descripciones de armas
 UPDATE raw.crime_data
-SET "Weapon Desc" = 'UNKNOWN WEAPON/OTHER WEAPON'
-WHERE "Weapon Used Cd" IS NULL AND "Weapon Desc" IS NULL;
+SET weapon_desc = UPPER(TRIM(weapon_desc));
 
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
 -- Limpieza de Status y Status Desc
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
--- Creación de la tabla de códigos de estado
-CREATE TABLE cleaning.status_codes (
-    status_code CHAR(2) PRIMARY KEY,
-    description VARCHAR(255)
-);
-
--- Insertar los códigos y descripciones de estado
-INSERT INTO cleaning.status_codes (status_code, description) VALUES
-('AA', 'Adult Arrest'),
-('IC', 'Invest Cont'),
-('JA', 'Juv Arrest'),
-('AO', 'Adult Other');
-
--- Actualizar registros donde no hay descripción de estado
+-- Actualizar la descripción del estado 'CC' de 'UNK' a 'UNKNOWN'
 UPDATE raw.crime_data
-SET "Status Desc" = (
-    SELECT description
-    FROM cleaning.status_codes
-    WHERE status_code = raw.crime_data."Status"
-)
-WHERE "Status Desc" IS NULL AND "Status" IS NOT NULL;
-
-/* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
--- Limpieza de Crm Cd 2
-/* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
--- Actualizar 'Crm Cd 2' para establecer -1 donde no hay registro
-UPDATE raw.crime_data
-SET "Crm Cd 2" = COALESCE("Crm Cd 2", -1)
-WHERE "Crm Cd 2" IS NULL;
+SET status_desc = 'UNKNOWN'
+WHERE status = 'CC' AND status_desc = 'UNK';
 
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
 -- Limpieza de LOCATION
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++ */
--- Actualizar 'LOCATION' para eliminar espacios extras y estandarizar la escritura
+
+-- Actualizar 'location' para eliminar espacios múltiples
 UPDATE raw.crime_data
-SET "LOCATION" = REGEXP_REPLACE(TRIM("LOCATION"), '\s+', ' ', 'g');
+SET location = REGEXP_REPLACE(TRIM(location), '\s+', ' ', 'g');
 
 -- Adicionalmente, para estandarizar partes comunes de direcciones
 UPDATE raw.crime_data
